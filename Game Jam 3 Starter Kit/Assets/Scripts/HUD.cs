d5b6954ae8c3e99520dc41d3d36fd62ddbdec453
Mark-Duda <144268612+Mark-Duda@users.noreply.github.com>
2024-05-06 10:56:58 -0700

    adasd

---
+++ Game Jam 3 Starter Kit/Assets/Scripts/HUD.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEditor;
+
+public class HUD : MonoBehaviour
+{
+    public TextMeshProUGUI coin;
+    public int gold;
+    public LevelLoader levelloader;
+    public int starttime;
+    public int timer;
+    public float time;
+    public TextMeshProUGUI gametime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        starttime = 180;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        coin.text = ""+gold;
+        time+= Time.deltaTime;
+
+        timer = starttime- Mathf.RoundToInt(time);
+        gametime.text = timer+"s";
+        if (timer < 1)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        timer = 0;
+        levelloader.LoadMap();
+        //reload scenez
+    }
+    
+}
