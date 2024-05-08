31a781ecd5a243b43440a192187beadcbaaa214d
Mark-Duda <144268612+Mark-Duda@users.noreply.github.com>
2024-05-08 11:17:24 -0700

    Merge branch 'main' of https://github.com/Mark-Duda/ninja-game

---
+++ Game Jam 3 Starter Kit/Assets/Scripts/Box.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using Color = UnityEngine.Color;
+
+public class Box : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Vector3 spawnpoint;
+    public Collider2D boxcollider;
+    public GameObject coin;
+    public float raycastLength = .1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxcollider = GetComponent<Collider2D>();
+        spawnpoint = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector2 rayStartl = new Vector2(boxcollider.bounds.min.x, boxcollider.bounds.min.y - .001f);
+        Vector2 rayStartr = new Vector2(boxcollider.bounds.max.x, boxcollider.bounds.min.y - .001f);
+        Vector2 rayStartm = new Vector2(boxcollider.bounds.center.x, boxcollider.bounds.min.y - .001f);
+
+        //Cast a ray downwards to check if the player is grounded
+        RaycastHit2D hitm = Physics2D.Raycast(rayStartm, Vector2.down, raycastLength);
+        RaycastHit2D hitr = Physics2D.Raycast(rayStartr, Vector2.down, raycastLength);
+        RaycastHit2D hitl = Physics2D.Raycast(rayStartl, Vector2.down, raycastLength);
+        if (hitr.collider != null && hitr.collider.CompareTag("Player")||hitl.collider != null && hitl.collider.CompareTag("Player")||hitm.collider != null && hitm.collider.CompareTag("Player"))
+        {
+            Instantiate(coin, spawnpoint, Quaternion.identity);
+            spriteRenderer.color=Color.gray;
+            enabled = false;
+        }
+    }
+}
---
+++ Game Jam 3 Starter Kit/Assets/Scripts/Enemy.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float movementSpeed = 2f;
+    public bool facingRight = true;
+    public Vector2 groundCheck;
+    public LayerMask groundLayer;
+    public float groundCheckDistance = 0.01f;
+    public Vector2 wallCheck;
+    public float wallCheckDistance = 0.01f;
+    public Collider2D collider;
+    private Rigidbody2D rb;
+    public bool isBlocked;
+    public bool isGrounded;
+    void Start()
+    {
+        collider = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+   
+        if (!facingRight)
+        {
+            groundCheck = new Vector2(collider.bounds.min.x, collider.bounds.min.y);   
+            isGrounded = Physics2D.Raycast(groundCheck, Vector2.down, groundCheckDistance );
+            Debug.DrawLine(groundCheck,groundCheck + Vector2.down*groundCheckDistance ,Color.red);
+           wallCheck = new Vector2(collider.bounds.min.x-.2f, collider.bounds.center.y);
+            isBlocked = Physics2D.Raycast(wallCheck, Vector2.right, wallCheckDistance);
+            Debug.DrawLine(wallCheck, wallCheck + Vector2.right*wallCheckDistance,Color.red, wallCheckDistance);
+        }
+        else
+        {
+          
+            groundCheck = new Vector2(collider.bounds.max.x, collider.bounds.min.y);   
+            isGrounded = Physics2D.Raycast(groundCheck, Vector2.down, groundCheckDistance );
+            Debug.DrawLine(groundCheck,groundCheck + Vector2.down*groundCheckDistance ,Color.red);
+            wallCheck = new Vector2(collider.bounds.max.x+.2f, collider.bounds.center.y);
+            isBlocked = Physics2D.Raycast(wallCheck, Vector2.left, wallCheckDistance);
+           Debug.DrawLine(wallCheck, wallCheck + Vector2.left*wallCheckDistance,Color.red, wallCheckDistance);
+        }
+        // If there is no ground or there is a wall in front, turn around
+        if (!isGrounded || isBlocked)
+        {
+            Flip();
+        }
+
+        // Move the enemy in the current direction
+        rb.velocity = new Vector2(facingRight ? movementSpeed : -movementSpeed, rb.velocity.y);
+    }
+
+    void Flip()
+    {
+        isBlocked = false;
+        // Reverse the direction the enemy is facing
+        facingRight = !facingRight;
+
+        // Rotate the enemy to face the new direction
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
+    
+
+
+
